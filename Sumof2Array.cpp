acab08411452f0b81c6002584e9b562e7b45bf05
gitHub1akash <88146321+gitHub1akash@users.noreply.github.com>
2021-10-19 20:38:51 +0530

    Update Sumof2Array.cpp

--- Sumof2Array.cpp
+++ Sumof2Array.cpp
@@ -1,34 +1,34 @@
 #include <stdio.h>
 int main() {
-  int r, c,  i, j;
+  int row, c,  i, j;
   printf("Enter the number of rows \n ");
-  scanf("%d", &r);
+  scanf("%d", &row);
   printf("Enter the number of columns \n");
-  scanf("%d", &c);
- int a[r][c], b[r][c], sum[r][c];
-  printf("Enter elements of the first matrix:\n");
-  for (i = 0; i < r; i++) {
+  scanf("%d", &col);
+ int a[row][col], b[row][col], sum[row][col];
+  printf("Enter elements of the 1st matrix:\n");
+  for (i = 0; i < row; i++) {
   
-    for (j = 0; j < c; j++) 
+    for (j = 0; j < col; j++) 
       
       scanf("%d", &a[i][j]);
     }
 
-  printf("Enter elements of the 2nd matrix:\n");
-  for (i = 0; i < r; i++) {
+  printf("Enter elements of the 2nd matrix :\n");
+  for (i = 0; i < row; i++) {
   
-    for (j = 0; j < c;j++) 
+    for (j = 0; j < col;j++) 
       scanf("%d", &b[i][j]);
     }
 
-  for (i = 0; i < r; i++)
-    for (j = 0; j < c; j++) {
+  for (i = 0; i < row; i++)
+    for (j = 0; j < col; j++) {
       sum[i][j] = a[i][j] + b[i][j];
     }
 
-  printf("Sum of the two matrices: \n");
-  for (i = 0; i < r; i++) {
-    for (j = 0; j < c; j++) 
+  printf("Sum of the two matrices is: \n");
+  for (i = 0; i < row; i++) {
+    for (j = 0; j < col; j++) 
       printf("%d   ", sum[i][j]);
         printf("\n\n"); 
     }
